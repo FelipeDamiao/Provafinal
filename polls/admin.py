ffd1a7144742a2c8a371ee0a88f359dac088e403
FelipeDamiao <felipedamiao81@gmail.com>
2022-09-01 13:42:38 -0300

    Ação: Concluir a parte 07 do tutorial;
    Data: 01/09/2022

--- polls/admin.py
+++ polls/admin.py
@@ -2,8 +2,24 @@ from django.contrib import admin
 
 # Parte 02
 from .models import Question
-
-admin.site.register(Question)
 # Fim da parte 02
+
+# Parte 07
+from .models import Choice, Question
+# Fim da parte 07
+
+# Parte 07
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None,               {'fields': ['question_text']}),
+        ('Date information', {'fields': ['pub_date']}),
+    ]
+
+admin.site.register(Question, QuestionAdmin)
+# Fim da parte 07
+
+# Parte 07
+admin.site.register(Choice)
+# Fim da parte 07
 
 # Register your models here.
