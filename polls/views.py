8fdd33ad3234a5b3995b0c43a5592b1ca43e2311
FelipeDamiao <felipedamiao81@gmail.com>
2022-09-01 09:47:28 -0300

    Ação: Conseguir terminar a parte 03;
    Data: 01/09/2022

--- polls/views.py
+++ polls/views.py
@@ -1,4 +1,14 @@
+# Parte 03
+from django.http import Http404
+# Fim da parte 03
+
 from django.shortcuts import render
+
+# Parte 03
+from django.template import loader
+
+from .models import Question
+# Fim da parte 03
 
 # Parte 01
 from django.http import HttpResponse
@@ -7,5 +17,30 @@ from django.http import HttpResponse
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
 # Fim da parte 01
+
+# Parte 03
+def detail(request, question_id):
+    return HttpResponse("You're looking at question %s." % question_id)
+
+def results(request, question_id):
+    response = "You're looking at the results of question %s."
+    return HttpResponse(response % question_id)
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s." % question_id)
+def index(request):
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    template = loader.get_template('polls/index.html')
+    context = {
+        'latest_question_list': latest_question_list,
+    }
+    return HttpResponse(template.render(context, request))
+def detail(request, question_id):
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404("Question does not exist")
+    return render(request, 'polls/detail.html', {'question': question})
+# Fim da parte 03
 
 # Create your views here.
