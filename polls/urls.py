b5e69d307e75ae80097f0117ffe6312503359fcb
FelipeDamiao <felipedamiao81@gmail.com>
2022-09-01 10:57:24 -0300

    Ação: Conseguir terminar a parte 04;
    Data: 01/09/2022

--- polls/urls.py
+++ polls/urls.py
@@ -10,11 +10,21 @@ urlpatterns = [
 ]
 
 # Parte 03
+#app_name = 'polls'
+#urlpatterns = [
+    #path('', views.index, name='index'),
+    #path('<int:question_id>/', views.detail, name='detail'),
+    #path('<int:question_id>/results/', views.results, name='results'),
+    #path('<int:question_id>/vote/', views.vote, name='vote'),
+#]
+# Fim da parte 03
+
+# Parte 04
 app_name = 'polls'
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('<int:question_id>/', views.detail, name='detail'),
-    path('<int:question_id>/results/', views.results, name='results'),
+    path('', views.IndexView.as_view(), name='index'),
+    path('<int:pk>/', views.DetailView.as_view(), name='detail'),
+    path('<int:pk>/results/', views.ResultsView.as_view(), name='results'),
     path('<int:question_id>/vote/', views.vote, name='vote'),
 ]
-# Fim da parte 03
+# Fim da Parte 04
